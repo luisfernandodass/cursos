bd130047e0ea0811bf86bdaf12433636eb6b50ae
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-07-17 14:45:05 -0300

    up

---
+++ curso/exercicios-com-site-estilizado/js/scroll-animation.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll(".js-scroll");
+const windowMetade = window.innerHeight * 0.5;
+
+if (sections.length) {
+  function animaScroll() {
+    sections.forEach((item) => {
+      const sectionTop = item.getBoundingClientRect().top;
+      //console.log(sectionTop) // 10.6466
+      //console.log(Math.floor(sectionTop)) // 10
+      const isSectionVisible = sectionTop - windowMetade < 0;
+      if (isSectionVisible) {
+        item.classList.add("ativo");
+      }
+    });
+  }
+  animaScroll();
+
+  window.addEventListener("scroll", animaScroll);
+}
